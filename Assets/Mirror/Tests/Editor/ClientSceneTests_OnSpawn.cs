a94d5917eba8124ce72d8bf6a865a0e90e4757f8
James Frowen <jamesfrowendev@gmail.com>
2020-05-04 22:39:16 +0100

    tests for ClientSceneTests.OnSpawn temp

---
+++ Assets/Mirror/Tests/Editor/ClientSceneTests_OnSpawn.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Mirror.Tests
+{
+    public class ClientSceneTests_OnSpawn : ClientSceneTestsBase
+    {
+        // Applies Payload Correctly
+        // Applies Payload to existing object (if one exists
+        // Spawns Prefab from prefab Dictionary
+        // Spawns Prefab from Handler
+        // Spawns Scene object from spawnableObjects Dictionary
+        // gives errors when...
+        //   guid and sceneId is empty
+        //   cant find prefab/handler with assetId
+        //   cant find object with sceneId
+        //   failed to spawn prefab
+        [Test] [Ignore("Not Implemented")] public void DoesLotsOfStuffCorrectly() { }
+    }
+}
